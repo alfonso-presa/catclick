1ddb8b9280ea626d518e01c960423f9e741b5e90
Alfonso Presa <alfonso.presa@gmail.com>
2015-09-30 16:21:58 +0200

    feat(): Admin panel

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,3 +1,4 @@
+/* global angular */
 'use strict';
 
 /**
@@ -9,7 +10,7 @@
  */
 angular.module('styleSageApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.cats = [
+    $scope.cats = localStorage.cats ? JSON.parse(localStorage.cats) : [
       {name:'Frodo', img: '/images/gato.jpg', counter: 0},
       {name:'Bilbo', img: '/images/gato2.jpg', counter: 0},
       {name:'Garfield', img: '/images/gato2.jpg', counter: 0},
@@ -22,5 +23,19 @@ angular.module('styleSageApp')
     };
     $scope.select = function (cat) {
       $scope.cat = cat;
+    };
+    $scope.openAdmin = function () {
+      $scope.admin = true;
+      $scope.editCat = angular.extend({},$scope.cat);
+    };
+    $scope.save = function () {
+      $scope.admin = false;
+      $scope.cat.name = $scope.editCat.name;
+      $scope.cat.img = $scope.editCat.img;
+      $scope.cat.counter = $scope.editCat.counter;
+      localStorage.cats = JSON.stringify($scope.cats);
+    };
+    $scope.cancel = function () {
+      $scope.admin = false;
     };
   });
